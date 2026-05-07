4c16414534891625d02db827c4cdcec1437cdafc
LUIS FELIPE CARDOSO SILVA <luiscardoso2311@gmail.com>
2026-05-07 02:41:02 -0300

    corrigir url do backend para render

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://localhost:3001/api' });
+const api = axios.create({ baseURL: 'https://senac-backend-l38q.onrender.com/api' });
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
